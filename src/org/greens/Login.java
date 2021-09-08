2c52ea4f14de5f7fb868341b5948930025a5d570
Depak95 <deepakprasana.cloud94@gmail.com>
2021-09-08 10:16:33 +0530

    Login

--- src/org/greens/Login.java
+++ src/org/greens/Login.java
@@ -1,19 +1,19 @@
 package org.greens;
 
+import java.sql.DriverManager;
+
 public class Login {
 
 	public static void main(String[] args) {
-		
-		
-		
-		
-		
-		
-		
-		
-		System.out.println("Login page");
 
-		System.out.println("Hello");
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+
+		System.out.println("This is addition");
+
+		System.out.println(c);
+
 	}
 
 }
